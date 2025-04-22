a4cc16f1536a9d4a45efc8cecf1fd69c76d410ba
Ehsan Jahanara <ehsan.jnra@gmail.com>
2025-04-22 18:49:58 +0330

    content will be encode and decode

--- decoder.py
+++ decoder.py
@@ -4,5 +4,9 @@ import os
 os.chdir("./b64/files")
 files = os.listdir(".")
 for file in files:
+    content = open(file, "r").read()
+    newContent = base64.b64decode(content).decode()
     newFile = base64.b64decode(file)
     os.rename(file, newFile)
+    with open(newFile, "w") as f:
+        f.write(newContent)
--- encoder.py
+++ encoder.py
@@ -4,5 +4,9 @@ import os
 os.chdir("./b64/files")
 files = os.listdir(".")
 for file in files:
+    content = open(file, "r").read()
+    newContent = base64.b64encode(content.encode()).decode()
     newFile = base64.b64encode(file.encode()).decode()
     os.rename(file, newFile) 
+    with open(newFile, "w") as f:
+        f.write(newContent)
